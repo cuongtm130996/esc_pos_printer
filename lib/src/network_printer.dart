a1e66b92c726d2def27e66b137fd1e5dba4631fd
cuongtm2 <cuongtm2@smartosc.com>
2023-05-14 20:55:36 +0700

    Update library

--- lib/src/network_printer.dart
+++ lib/src/network_printer.dart
@@ -9,7 +9,7 @@
 import 'dart:io';
 import 'dart:typed_data' show Uint8List;
 import 'package:esc_pos_utils/esc_pos_utils.dart';
-import 'package:image/image.dart';
+import 'package:esc_pos_utils/image.dart';
 import './enums.dart';
 
 /// Network Printer
